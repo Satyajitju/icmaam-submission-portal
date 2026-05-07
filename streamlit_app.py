7b598814e527c699e484de876005fb6feef41cbe
Satyajitju <satya.jumath@gmail.com>
2026-05-07 23:22:48 +0530

    Fixed missing service_account import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
