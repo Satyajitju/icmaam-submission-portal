ddc6f7c21c0bf681d5db19815a6df39130824ba9
Satyajitju <satya.jumath@gmail.com>
2026-05-07 23:32:25 +0530

    Enabled supportsAllDrives for Drive API

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,6 +227,7 @@ def create_folder(folder_name, parent_id):
     folder = service.files().create(
         body=file_metadata,
         fields='id'
+        supportsAllDrives=True
     ).execute()
 
     return folder.get('id')
